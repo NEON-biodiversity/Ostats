c6863a36fea3573a39fc9ba2b1fb8608937dbc7c
yyue-r <63085051+yyue-r@users.noreply.github.com>
2020-07-06 13:56:02 -0400

    update ant_data documentation

---
+++ R/ant_data.R
@@ -0,0 +1,9 @@
+#' Sample Circular Data
+#'
+#' This is a sample dataset with discrete hourly data collection from a study
+#' about ant-mediated seed dispersal.
+#'
+#' @references Stuble, K.L., Patterson, C.M., Rodriguez-Cabal, M.A., Ribbons,
+#'   R.R., Dunn, R.R. & Sanders, N.J. (2014). Ant-mediated seed dispersal in a
+#'   warmed world. PeerJ, 2, e286. https://peerj.com/articles/286/
+"ant_data"
