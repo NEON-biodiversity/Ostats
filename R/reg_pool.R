54aebba80244f485d45f2235097a3aedba60c86e
yyue-r <63085051+yyue-r@users.noreply.github.com>
2020-07-09 16:18:52 -0400

    Updated Ostats_regional example and the data

---
+++ R/reg_pool.R
@@ -0,0 +1,7 @@
+#' Sample Data for Ostats_regional
+#'
+#' This is a sample dataset with small mammal data from the NEON sites BART and
+#' HARV across years.The two sites belong to the same domain, the Northeast domain.
+#'
+#' @references
+"reg_pool"
