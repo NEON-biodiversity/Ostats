67bb291aa20c23329452787a5132ae755db0c60d
Quentin Read <quentin.read@gmail.com>
2020-07-14 08:35:28 -0400

    Edit test for pairwise overlap

--- tests/testthat/test-ostatstest.R
+++ tests/testthat/test-ostatstest.R
@@ -5,18 +5,20 @@ library(sfsmisc)
 #Ostats example
 #how do I make it into a test?
 #attempt with pairwise_overlap
- a <- mtcars %>%
-   filter(cyl==6)
- a <- a[,1]
- b <- mtcars %>%
-   filter(cyl==4)
- b <- b[,1]
- result<- pairwise_overlap(a,b)
- result
 
- test_that (
-   "pairwise overlap works",
-   {
-     expect_equal((pairwise_overlap(a,b)),result)
-   }
- )
+# Test 1: pairwise overlap for two distributions
+
+n <- 1e7
+set.seed(111)
+a <- runif(n, min = 0, max = 2)
+b <- runif(n, min = 1, max = 3)
+
+result1 <- pairwise_overlap(a, b)
+expected1 <- c(overlap = 0.5, overlap_a = 0.5, overlap_b = 0.5)
+
+test_that (
+"pairwise overlap returns expected output",
+{
+  expect_equal(result1, expected1, tolerance = 0.01)
+}
+)
