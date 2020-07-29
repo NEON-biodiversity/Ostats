621a6a302b5a27bd0324fadfa17543363df8b033
isafluck <isaflucke@gmail.com>
2020-07-28 21:37:30 -0300

    updated Ostats_plot
    Now you can choose to plot all the sites or only selected ones.
    The number of colors was also fixed.

--- R/Ostats_plot.R
+++ R/Ostats_plot.R
@@ -8,9 +8,8 @@
 #'@param siteID Site identity: a column in indiv_dat data frame that indicates the names of each community.
 #'@param taxonID Taxon identity: a column in indiv_dat data frame that indicates species or taxa names.
 #'@param trait The trait you want to overlap among species: a column in indiv_dat data frame containing trait measurements for each individual.
-#'@param overlap_dat a data frame where the first column contains the names of each community and the second column contains the values of total overlap among species in each of these communities obtained from the \code{\link{Ostats}} function  NEED TO FIGURE OUT HOW TO USE OVERLAP OUTPUT
-#'@param trait_name a character that specifies the name of the trait, to be
-#'  labelled on the x axis.
+#'@param overlap_dat an object containing the results from the Ostats function - see \code{\link{Ostats}}
+#'@param sites2use a vector that select the sites you want to plot. If NULL, the function will plot all the sites.
 #'
 #'
 #'@return Density plots of species trait distribution plotted on the same graph
@@ -20,88 +19,129 @@
 #'  pairwise niche overlap statistics)
 #'
 #'@examples
-#'
-#'
-#' #example use same dataset as ostats (need to change later)
-#'
+#'library(tidyverse)
 #'library(ggplot2)
 #'library(dplyr)
 #'
-#'overlap_dat <- Ostats_bysite2015 #results of ostat function
-#'final_mammal_data <- read.csv('final_NEON_mammal_data.csv', stringsAsFactors = FALSE) #raw data
+#'overlap_dat <- Ostats_example #your results from the Ostat function - see \code{\link{Ostats}}
+#'indiv_dat <- read_csv('https://ndownloader.figshare.com/files/9167548', col_names = T)
+#'siteID <- indiv_dat$siteID
+#'taxonID <- indiv_dat$taxonID
+#'trait <- indiv_dat$logweight
+#'#to plot only selected sites:
+#'sites2use<- c('BART','KONZ','JORN') #used in the example
 #'
-#'indiv_dat <-data.frame(final_mammal_data$siteID, final_mammal_data$taxonID, final_mammal_data$weight)
-#'siteID <- indiv_dat$final_mammal_data.siteID
-#'taxonID <- indiv_dat$final_mammal_data.taxonID
-#'trait <- log(indiv_dat$final_mammal_data.weight)
-#'
-#'
-#'Ostats_plot(indiv_dat = indiv_dat, siteID = siteID, taxonID = taxonID, trait = trait, overlap_dat = overlap_dat)
+#'#to plot all sites:
+#'sites2use<- NULL
+
+#'Ostats_plot(indiv_dat = indiv_dat, siteID = siteID, taxonID = taxonID, trait = trait, overlap_dat = overlap_dat, sites2use = sites2use)
 #'@export
 #'
 Ostats_plot<-function(indiv_dat, siteID, taxonID, trait, overlap_dat, sites2use=NULL){
 
-  #put a default that sites2use is null
+
+  if(is.null(sites2use)==FALSE){
+
+    ostat_norm<-overlap_dat$overlaps_norm
+    ostat_norm<-subset(ostat_norm,rownames(ostat_norm)%in%sites2use)
+    indiv_dat<-subset(indiv_dat, indiv_dat$siteID%in%sites2use)
+    siteID <- indiv_dat$siteID
+    taxonID <- indiv_dat$taxonID
+    trait <- indiv_dat$logweight
 
 
-  ostat_norm<-overlap_dat$overlaps_norm
-  #colorvalues <- sample(hcl.colors(10, palette = 'viridis'), size = length(unique(taxonID)), replace = TRUE) #it does not work with 26, it asks for 50, but I dont know why
-  colorvalues <- sample(hcl.colors(10, palette = 'viridis'), size = 50, replace = TRUE)
-  theme_set(
-    theme_bw() + theme(panel.grid = element_blank(),
-                       axis.text = element_text(size = 12),
-                       axis.title = element_text(size = 18),
-                       axis.text.y=element_blank(),
-                       axis.ticks.y=element_blank(),
-                       legend.position = 'none',
-                       strip.background = element_blank()))
+    colorvalues <- sample(hcl.colors(10, palette = 'viridis'), size = length(unique(taxonID)), replace = TRUE)
+    theme_set(
+      theme_bw() + theme(panel.grid = element_blank(),
+                         axis.text = element_text(size = 12),
+                         axis.title = element_text(size = 18),
+                         axis.text.y=element_blank(),
+                         axis.ticks.y=element_blank(),
+                         legend.position = 'none',
+                         strip.background = element_blank()))
 
 
 
-  rownames(ostat_norm)
-  positions<-factor(match(siteID, rownames(ostat_norm)))
 
-  M<-matrix(data = NA, nrow = length(siteID), ncol = 3)
-  M[,1]<-siteID
-  M[,2]<-factor(positions)
-  M<-data.frame(M)
+    positions<-factor(match(indiv_dat$siteID, rownames(ostat_norm)))
 
-  for(i in 1:nlevels(positions)){
+    M<-matrix(data = NA, nrow = length(siteID), ncol = 3)
+    M[,1]<-siteID
+    M[,2]<-factor(positions)
+    M<-data.frame(M)
 
-    M$X3[M$X2==i]<-ostat_norm[i,]
+    for(i in 1:nlevels(positions)){
+
+      M$X3[M$X2==i]<-ostat_norm[i,]
+    }
+
+    Ovalues1<-M[,3]
+    Ovalues1[is.na(Ovalues1)] = 0
+    Ovalues2<-round(x = as.numeric(Ovalues1),digits=4)
+    Ovalues3<-cbind(siteID, Ovalues2)
+    Ovalues<-paste(Ovalues3[,1], Ovalues3[,2], sep="_")
+    indiv_dat$Ovalues<-Ovalues
+
+
+    ggplot(indiv_dat %>% mutate(siteID = factor(siteID))) +
+      stat_density(adjust = 2, size = 1,aes(x = trait, group = taxonID, fill=taxonID), alpha = 0.5, geom='polygon', position = 'identity') +
+      facet_wrap(~factor(Ovalues), ncol = 3) +
+      scale_fill_manual(values = colorvalues) +
+      scale_x_continuous(name = colnames(ostat_norm), limits = c(0.5*min(trait,na.rm=TRUE), 1.5*max(trait,na.rm=TRUE))) +
+      scale_y_continuous(name = 'Probability Density',expand = c(0,0))
+
+
+
+
+
+  } else {
+
+    ostat_norm<-overlap_dat$overlaps_norm
+    colorvalues <- sample(hcl.colors(10, palette = 'viridis'), size = length(unique(taxonID)), replace = TRUE)
+    theme_set(
+      theme_bw() + theme(panel.grid = element_blank(),
+                         axis.text = element_text(size = 12),
+                         axis.title = element_text(size = 18),
+                         axis.text.y=element_blank(),
+                         axis.ticks.y=element_blank(),
+                         legend.position = 'none',
+                         strip.background = element_blank()))
+
+
+
+
+    positions<-factor(match(siteID, rownames(ostat_norm)))
+
+    M<-matrix(data = NA, nrow = length(siteID), ncol = 3)
+    M[,1]<-siteID
+    M[,2]<-factor(positions)
+    M<-data.frame(M)
+
+    for(i in 1:nlevels(positions)){
+
+      M$X3[M$X2==i]<-ostat_norm[i,]
+    }
+
+    Ovalues1<-M[,3]
+    Ovalues1[is.na(Ovalues1)] = 0
+    Ovalues2<-round(x = as.numeric(Ovalues1),digits=4)
+    Ovalues3<-cbind(siteID, Ovalues2)
+    Ovalues<-paste(Ovalues3[,1], Ovalues3[,2], sep="_")
+    indiv_dat$Ovalues<-Ovalues
+
+
+
+
+    ggplot(indiv_dat %>% mutate(siteID = factor(siteID))) +
+      stat_density(adjust = 2, size = 1,aes(x = trait, group = taxonID, fill=taxonID), alpha = 0.5, geom='polygon', position = 'identity') +
+      facet_wrap(~factor(Ovalues), ncol = 3) +
+      scale_fill_manual(values = colorvalues) +
+      scale_x_continuous(name = colnames(ostat_norm), limits = c(0.5*min(trait,na.rm=TRUE), 1.5*max(trait,na.rm=TRUE))) +
+      scale_y_continuous(name = 'Probability Density',expand = c(0,0))
+
+
+
+
   }
 
-  Ovalues1<-M[,3]
-  Ovalues1[is.na(Ovalues1)] = 0
-  Ovalues2<-round(x = as.numeric(Ovalues1),digits=4)
-  Ovalues3<-cbind(siteID, Ovalues2)
-  Ovalues<-paste(Ovalues3[,1], Ovalues3[,2], sep="_")
-  indiv_dat$Ovalues<-Ovalues
-
-  #if(is.null(sites2use)==TRUE){
-  #PLOT THEM ALL
-
-  ggplot(indiv_dat %>% mutate(siteID = factor(siteID))) +
-    stat_density(adjust = 2, size = 1,aes(x = trait, group = taxonID, fill=taxonID), alpha = 0.5, geom='polygon', position = 'identity') +
-    facet_wrap(~factor(Ovalues), ncol = 3) +
-    scale_fill_manual(values = colorvalues) +
-    scale_x_continuous(name = colnames(ostat_norm), limits = c(0.5*min(trait,na.rm=TRUE), 1.5*max(trait,na.rm=TRUE))) +
-    scale_y_continuous(name = 'Probability Density',expand = c(0,0))
-
-  #geom_text(aes(label = paste('Overlap =', round(ostat_norm,3)), x = 1.5, y = 8.5), color = 'black', data = data.frame(ostat_norm) %>% mutate(siteID = levels(factor(siteID))))
-
-
-  #} else {
-  #PLOT ONLY SOME OF THEM
-
-  #ggplot(filter(indiv_dat, siteID %in% sites2use) %>% mutate(siteID = factor(siteID, levels=sites2use)))+
-  #stat_density(adjust = 2, size = 1,aes(x = trait, group = taxonID, fill=taxonID), alpha = 0.5, geom='polygon', position = 'identity') +
-  #facet_wrap(~ siteID, ncol = 3) +
-  #scale_fill_manual(values = colorvalues) +
-  #scale_x_continuous(name = colnames(ostat_norm), limits = c(0.5*min(trait,na.rm=TRUE), 1.5*max(trait,na.rm=TRUE))) +
-  #scale_y_continuous(name = 'Probability Density',expand = c(0,0)) +
-  #geom_text(aes(label = paste('Overlap =', round(ostat_norm,3)), x = 1.5, y = 8.5), color = 'black', data = overlap_dat %>% filter(siteID %in% sites2use) %>% mutate(siteID = factor(siteID, levels=sites2use)))
-
-
 }
-
