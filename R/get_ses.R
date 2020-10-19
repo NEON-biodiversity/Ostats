071fdda475cb01f3889b44a1cc568680eeee8b02
yyue-r <63085051+yyue-r@users.noreply.github.com>
2020-10-19 11:24:03 -0400

    fixed long line issue (Annie's feedback); updated vignette.

--- R/get_ses.R
+++ R/get_ses.R
@@ -1,21 +1,40 @@
-#' @title  Calculation of standardized effect sizes of overlap statistics against null distributions
+#'@title  Calculation of standardized effect sizes of overlap statistics against
+#'  null distributions
 #'
-#' @description Extract quantiles to get standardized effect sizes for the overlap stats
+#'@description Extract quantiles to get standardized effect sizes for the
+#'  overlap stats
 #'
-#' @details Implementation of null model that swaps means of distributions to test whether distributions are more evenly spaced than expected by chance - It is part of the Ostats function
+#'@details Implementation of null model that swaps means of distributions to
+#'  test whether distributions are more evenly spaced than expected by chance -
+#'  it is part of the Ostats function
 #'
-#' @seealso \code{\link{Ostats}} to Calculate O-statistics (community-level pairwise niche overlap statistics)
+#'@seealso \code{\link{Ostats}} to Calculate O-statistics (community-level
+#'  pairwise niche overlap statistics)
 #'
-#' @param o an matrix data containing the overlapping values of normal distribution of the observed data (the observed local O-stats for each community)
-#' @param o_n an array object containing the overlapping values of normal distribution of the randomize data - (the null distributions of O-stats function)
-#' @param qs quantiles of the null distribution to extract, usually 0.025 and 0.975
+#'@param o an matrix data containing the overlapping values of normal
+#'  distribution of the observed data (the observed local O-stats for each
+#'  community)
+#'@param o_n an array object containing the overlapping values of normal
+#'  distribution of the randomize data - (the null distributions of O-stats
+#'  function)
+#'@param qs quantiles of the null distribution to extract, usually 0.025 and
+#'  0.975
 #'
-#' @return
-#' \item{ses}{a matrix containing values of standardized effect sizes for observed data of each of #' the locals (rows) and traits(columns) avaliated. To see if the overlap values can be expected by #' chance see returns ses_upper and ses_lower. If the value corresponding to the local in ses is between ses_upper and ses_lower, the overlapping between traits in community can be expected by chance.}
-#' \item{ses_lower}{a matrix containing null values extracted for the quantil of qs[1] (usually 0.025)}
-#' \item{ses_upper}{a matrix containing null values extracted for the quantil of qs[2] (usually 0.975)}
-#' \item{raw_lower}{a matrix containing null values extracted for the quantil of qs[1] (usually 0.025) without subtracting the mean and divided by standard deviation (see equation for calculating the standardize effect size on the function code)}
-#' \item{raw_upper}{a matrix containing null values extracted for the quantil of qs[2] (usually 0.975) without subtracting the mean and divided by standard deviation (see equation for calculating the standardize effect size on the function code)}
+#'@return \item{ses}{a matrix containing values of standardized effect sizes for
+#'observed data of each of #' the locals (rows) and traits(columns) avaliated.
+#'To see if the overlap values can be expected by #' chance see returns
+#'ses_upper and ses_lower. If the value corresponding to the local in ses is
+#'between ses_upper and ses_lower, the overlapping between traits in community
+#'can be expected by chance.} \item{ses_lower}{a matrix containing null values
+#'extracted for the quantil of qs[1] (usually 0.025)} \item{ses_upper}{a matrix
+#'containing null values extracted for the quantil of qs[2] (usually 0.975)}
+#'\item{raw_lower}{a matrix containing null values extracted for the quantil of
+#'qs[1] (usually 0.025) without subtracting the mean and divided by standard
+#'deviation (see equation for calculating the standardize effect size on the
+#'function code)} \item{raw_upper}{a matrix containing null values extracted for
+#'the quantil of qs[2] (usually 0.975) without subtracting the mean and divided
+#'by standard deviation (see equation for calculating the standardize effect
+#'size on the function code)}
 #'
 #'@examples
 #' #import data
@@ -44,9 +63,13 @@
 #' ###set the "o" argument - the overlap between observed data
 #' for (s in 1:nlevels(plots)) {
 #'  for (t in 1:ncol(traits)) {
-#'    overlap_norm_st <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t], sp = sp[plots == levels(plots)[s]], data_type ="linear"), TRUE)
+#'    overlap_norm_st <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t],
+#'                                                   sp = sp[plots == levels(plots)[s]],
+#'                                                   data_type ="linear"), TRUE)
 #'    overlaps_norm[s, t] <- if (inherits(overlap_norm_st, 'try-error')) NA else overlap_norm_st
-#'    overlap_unnorm_st <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t], sp = sp[plots == levels(plots)[s]], data_type ="linear"), TRUE)
+#'    overlap_unnorm_st <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t],
+#'                                                     sp = sp[plots == levels(plots)[s]],
+#'                                                     data_type ="linear"), TRUE)
 #'    overlaps_unnorm[s, t] <- if (inherits(overlap_unnorm_st, 'try-error')) NA else overlap_unnorm_st
 #'  }
 #' }
@@ -59,8 +82,15 @@
 #' for (i in 1:nperm) {
 #'  for (s in 1:nlevels(plots)) {
 #'    for (t in 1:ncol(traits)) {
-#'      if (!shuffle_weights & !swap_means) overlap_norm_sti <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t], sp = sample(sp[plots == levels(plots)[s]]), data_type = "linear"), TRUE)
-#'      if (shuffle_weights) overlap_norm_sti <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t], sp = sp[plots == levels(plots)[s]], norm=TRUE, randomize_weights = TRUE, data_type = "linear"), TRUE)
+#'      if (!shuffle_weights & !swap_means)
+#'      overlap_norm_sti <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t],
+#'                                                      sp = sample(sp[plots == levels(plots)[s]]),
+#'                                                      data_type = "linear"), TRUE)
+#'      if (shuffle_weights)
+#'      overlap_norm_sti <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s], t],
+#'                                                      sp = sp[plots == levels(plots)[s]],
+#'                                                      randomize_weights = TRUE,
+#'                                                      data_type = "linear"), TRUE)
 #'      if (swap_means) {
 #'        traits_st <- traits[plots==levels(plots)[s], t]
 #'        sp_st <- sp[plots==levels(plots)[s]]
@@ -72,18 +102,27 @@
 #'        traitmeans_null <- sample(traitmeans)
 #'        sp_null <- rep(names(traitmeans_null), table(sp_st))
 #'        traits_null <- traitdeviations + traitmeans_null[sp_null]
-#'        overlap_norm_sti <- try(community_overlap_merged(traits = traits_null, sp = sp_null, norm =TRUE, randomize_weights = FALSE, data_type = "linear"), TRUE)
+#'        overlap_norm_sti <- try(community_overlap_merged(traits = traits_null,
+#'                                                        sp = sp_null,
+#'                                                        normal =TRUE,
+#'                                                        randomize_weights = FALSE,
+#'                                                        data_type = "linear"), TRUE)
 #'      }
 #'
-#'      overlaps_norm_null[s, t, i] <- if (inherits(overlap_norm_sti, 'try-error')) NA else overlap_norm_sti
-#'      overlap_unnorm_sti <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s] , t], sp = sample(sp[plots == levels(plots)[s]]), data_type = "linear"), TRUE)
-#'      overlaps_unnorm_null[s, t, i] <- if (inherits(overlap_unnorm_sti, 'try-error')) NA else overlap_unnorm_sti
+#'      overlaps_norm_null[s, t, i] <- if (inherits(overlap_norm_sti, 'try-error')) NA
+#'                                     else overlap_norm_sti
+#'      overlap_unnorm_sti <- try(community_overlap_merged(traits = traits[plots == levels(plots)[s] , t],
+#'                                                        sp = sample(sp[plots == levels(plots)[s]]),
+#'                                                        data_type = "linear"), TRUE)
+#'      overlaps_unnorm_null[s, t, i] <- if (inherits(overlap_unnorm_sti, 'try-error')) NA
+#'                                       else overlap_unnorm_sti
 #'    }
 #'  }
 #' }
 #'
 #'
 #' SES<-get_ses(o = overlaps_norm,o_n =overlaps_norm_null ,qs = c(0.025, 0.975))
+
 get_ses <- function(o, o_n, qs) {
   ses <- ses_lower <- ses_upper <- raw_lower <- raw_upper <- matrix(NA, nrow=nrow(o), ncol=ncol(o))
 
