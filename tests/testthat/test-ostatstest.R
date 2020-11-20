c188a81179203e008d553958b59734af7485c587
Quentin Read <quentin.read@gmail.com>
2020-11-19 19:46:54 -0500

    correct the first testthat and add local data to vignette

--- tests/testthat/test-ostatstest.R
+++ tests/testthat/test-ostatstest.R
@@ -11,7 +11,7 @@ a <- runif(n, min = 0, max = 2)
 b <- runif(n, min = 1, max = 3)
 
 result1 <- pairwise_overlap(a, b)
-expected1 <- c(overlap = 0.5, overlap_a = 0.5, overlap_b = 0.5)
+expected1 <- c(overlap_average = 0.5, overlap_a = 0.5, overlap_b = 0.5)
 
 test_that (
 "pairwise overlap returns expected output",
