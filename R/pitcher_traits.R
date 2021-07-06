2d8d1ca212208e3e029ad017c25bfe4176d03675
Sydne Record (she/her/hers) <sydnerecord@users.noreply.github.com>
2021-07-06 12:03:12 -0400

    Update pitcher_traits.R

--- R/pitcher_traits.R
+++ R/pitcher_traits.R
@@ -1,6 +1,7 @@
 #' Sample Multivariate Trait Data (Pitcher Plants)
 #'
-#' Insert documentation here. Lines should be no more than 80 characters long.
+#' These data represent trait information collected on carnivorous pitchers of the Northern Pitcher plant, *Sarracenia purpurea*, at 36 sites across the species native North American latitudinal range 30.197 $^\circ$N – 53.722 $^\circ$N (Freedman et al. 2021). Pitcher traits measured include orthogonal rosette diameters, pitcher length, pitcher width, keel width, mouth diameter, and lip thickness. See Ellison and Gotelli (2002) for a diagram illustrating traits measured.
 #'
-#' @references Insert reference here, if there is a publication. Otherwise delete this line.
-"pitcher_traits"
+#' @references 
+#' Ellison, A.M., and N.J. Gotelli. 2002. Nitrogen availability alters the expression of carnivory in the Northern Pitcher Plant, *Sarracenia purpurea*. *Proceedings of the National Academy of Science* 99(7):4409-12.
+#' Freedman, Z.B., A. McGrew, B. Baiser, M. Besson, D. Gravel, T. Poisot, S. Record, L.B. Trotta, and N.J. Gotelli. 2021. Environment-host-microbial interactions shape the *Sarracenia purpurea* microbiome at the continental scale. *Ecology*, 102(5):e03308.
