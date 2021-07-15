afdf90f31b5760e6040e1a2478109375ce70e9a6
Quentin Read <quentin.read@gmail.com>
2021-07-15 09:46:17 -0400

    Rename pitcher_traits_subset to pitcher_traits

--- R/pitcher_traits.R
+++ R/pitcher_traits.R
@@ -1,6 +1,6 @@
 #' Sample Multivariate Trait Data (Pitcher Plants)
 #'
-#' These data represent trait information collected on carnivorous pitchers of the Northern Pitcher plant, *Sarracenia purpurea*, at 36 sites across the species native North American latitudinal range 30.197°N – 53.722°N (Freedman et al. 2021). Pitcher traits measured include orthogonal rosette diameters, pitcher length, pitcher width, keel width, mouth diameter, and lip thickness. See Ellison and Gotelli (2002) for a diagram illustrating traits measured.
+#' These data represent a subset of trait information collected on carnivorous pitchers of the Northern Pitcher plant, *Sarracenia purpurea*, across the species native North American latitudinal range 30.197°N – 53.722°N (Freedman et al. 2021). We provide data from 5 sites out of the 36 sites sampled. Pitcher traits measured include orthogonal rosette diameters, pitcher length, pitcher width, keel width, mouth diameter, and lip thickness. See Ellison and Gotelli (2002) for a diagram illustrating traits measured.
 #'
 #' @references
 #' Ellison, A.M., and N.J. Gotelli. 2002. Nitrogen availability alters the expression of carnivory in the Northern Pitcher Plant, *Sarracenia purpurea*. *Proceedings of the National Academy of Science* 99(7):4409-12.
